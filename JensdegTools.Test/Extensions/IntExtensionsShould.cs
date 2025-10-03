76531756c2cb4590f2da35feb941f8b5cad22304
jensdeg <jensdegraaf88@gmail.com>
2025-10-04 00:35:42 +0200

    tests

---
+++ JensdegTools.Test/Extensions/IntExtensionsShould.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+using JensdegTools.Extensions;
+
+namespace JensdegTools.Test.Extensions
+{
+    public class IntExtensionsShould
+    {
+        [Fact]
+        public void ConvertNumberToLetters()
+        {
+            // Arrange
+            var expectedResult = "a";
+            var expectedResult2 = "abcdefghij";
+
+            // Act
+            var result = 1.ToLetters();
+            var result2 = 1234567890.ToLetters();
+
+            // Assert
+            result.Should().NotBeEmpty();
+            result.Should().Be(expectedResult);
+            result2.Should().Be(expectedResult2);
+        }
+    }
+}
